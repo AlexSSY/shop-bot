3bda52935aa03d50b9219a33d0be7113def1f383
AlexSSY <adolf5341@gmail.com>
2025-08-12 17:26:35 +0300

    fix /add command

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from pathlib import Path
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.state import StatesGroup, State
@@ -28,35 +28,35 @@ async def start(message: Message):
 
 
 @dp.message(Command('add'))
-async def add_new_item(message: Message, fsm: FSMContext):
-    await fsm.set_state(AddProductStates.name)
+async def add_new_item(message: Message, state: FSMContext):
+    await state.set_state(AddProductStates.name)
     await message.answer("Enter item's name")
 
 
 @dp.message(AddProductStates.name)
-async def process_add_product_name(message: Message, fsm: FSMContext):
+async def process_add_product_name(message: Message, state: FSMContext):
     name = message.text.strip()
-    await fsm.update_data(name=name)
-    await fsm.set_state(AddProductStates.image_url)
+    await state.update_data(name=name)
+    await state.set_state(AddProductStates.image_url)
     await message.answer("Send to me item's image")
 
 
 @dp.message(AddProductStates.image_url)
-async def process_add_product_image(message: Message, fsm: FSMContext):
+async def process_add_product_image(message: Message, state: FSMContext):
     image_url = message.photo[-1].file_id
-    await fsm.update_data(image_url=image_url)
-    await fsm.set_state(AddProductStates.price)
+    await state.update_data(image_url=image_url)
+    await state.set_state(AddProductStates.price)
     await message.answer("Send to me item's price")
 
 
 @dp.message(AddProductStates.price)
-async def process_add_product_price(message: Message, fsm: FSMContext):
+async def process_add_product_price(message: Message, state: FSMContext):
     price = float(message.text)
-    await fsm.update_data(price=price)
-    item_data = await fsm.get_data()
+    await state.update_data(price=price)
+    item_data = await state.get_data()
     await crud.add_product(**item_data)
     await message.answer('Product successfully added in to shop.')
-    await fsm.clear()
+    await state.clear()
 
 
 async def main():
