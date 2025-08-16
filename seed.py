9ea710c938651b593ac327d882d87a56db24ab40
AlexSSY <adolf5341@gmail.com>
2025-08-16 16:49:40 +0300

    make price much shorter

--- seed.py
+++ seed.py
@@ -18,7 +18,7 @@ async def seed():
         await crud.add_product(
             name=fake.name_nonbinary(),
             image_id='AgACAgIAAxkBAAMhaJtOO4COGRLHm325QKha7bn9i8cAAkkDMhum1eBIOsMjyypju6gBAAMCAAN5AAM2BA',
-            price=random.random()
+            price=round(random.random(), 2)
         )
 
 
